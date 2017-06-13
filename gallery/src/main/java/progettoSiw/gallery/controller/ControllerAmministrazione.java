0dbc7277701b4f1fa8c99ec0dec2bf144823a77a
Lorenzo Catalli <lorenzo.catalli@gmail.com>
2017-06-13 23:59:24 +0200

    vistaOpera vistaArtista (non ancora complete)

--- gallery/src/main/java/progettoSiw/gallery/controller/ControllerAmministrazione.java
+++ gallery/src/main/java/progettoSiw/gallery/controller/ControllerAmministrazione.java
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-package progettoSiw.gallery.controller;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import progettoSiw.gallery.model.Autore;
-import progettoSiw.gallery.service.AutoreService;
-import progettoSiw.gallery.service.OperaService;
-
-
-@Controller
-public class ControllerAmministrazione {
-
-	@Autowired
-	private AutoreService autoreService;
-	
-
-	@GetMapping("/formAutore")
-    public String formAutore(Model model) {
-		return "formAutore";
-    }
-	
-	@GetMapping("/formOpera")
-    public String formOpera( Model model) {
-		Iterable<Autore> autori=this.autoreService.findAll();
-    	model.addAttribute("autori",autori);
-        return "formOpera";
-    }
-	
-	@GetMapping("/formRimozione")
-    public String formRimozione(Model model) {
-		//TODO reindirizzamento e modello da mettere a disp
-		Iterable<Autore> autori=this.autoreService.findAll();
-    	model.addAttribute("autori",autori);
-        return "listaAutori";
-    }
-}
-=======
 package progettoSiw.gallery.controller;
 
 
@@ -82,4 +37,3 @@ public class ControllerAmministrazione {
         return "listaAutori";
     }
 }
->>>>>>> refs/remotes/origin/master
